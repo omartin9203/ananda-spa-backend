2ab9918e7b83a13ea8b2cd9c5f7f9a1ae9f2c35b
Roilan <armenterosroilan@gmail.com>
2020-05-13 16:35:37 -0400

    adding google scrape function

--- src/application/common/services/review/review.service.ts
+++ src/application/common/services/review/review.service.ts
@@ -89,4 +89,59 @@ export class ReviewService extends ResourceService<ReviewDto> {
     async getReviewsPerDirectory(filter: any = {}, sort: any = {}, skip = 0, limit = 10) {
         return await this.repository.getReviewsPerDirectory(filter, sort, skip, limit);
     }
+
+    async googleScrape() {
+        let scrape = async () => { // Prepare scrape...
+            try {
+                const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disabled-setuid-sandbox'] }); // Prevent non-needed issues for *NIX
+                const page = await browser.newPage(); // Create request for the new page to obtain...
+
+                // Replace with your Google Maps URL... Or Test the Microsoft one...
+                //await page.goto('https://www.google.com/maps/place/Microsoft/@36.1275216,-115.1728651,17z/data=!3m1!5s0x80c8c416a26be787:0x4392ab27a0ae83e0!4m7!3m6!1s0x80c8c4141f4642c5:0x764c3f951cfc6355!8m2!3d36.1275216!4d-115.1706764!9m1!1b1');
+
+                await page.goto('https://www.google.com/maps/place/Ananda+Spa/@25.7679707,-80.3666096,17z/data=!4m7!3m6!1s0x88d9b97e68d33533:0xe5f95c7e2dd8f33a!8m2!3d25.7679659!4d-80.3644209!9m1!1b1?hl=es&authuser=0'); // Define the Maps URL to Scrape...
+                await page.waitFor(1000); // In case Server has JS needed to be loaded...
+
+                const result = await page.evaluate(() => { // Let's create variables and store values...
+                    let reviews = [];
+
+                    var divs = document.querySelectorAll('.section-review-content'), i;
+
+                    for (i = 0; i < divs.length; ++i) {
+                        let reviewJson = { fullName: '', postDate: '', starRating: '', postReview: '' };
+
+
+                        reviewJson.fullName = (<HTMLElement>divs[i].querySelector('.section-review-title')).innerText; // Full Name
+                        reviewJson.postDate = (<HTMLElement>divs[i].querySelector('.section-review-publish-date')).innerText; // Date Posted
+                        let Rating = divs[i].querySelector('.section-review-stars').getAttribute("aria-label"); // Star Rating
+                        reviewJson.postReview = (<HTMLElement>divs[i].querySelector('.section-review-text')).innerText; // Review Posted by Full Name aka Poster
+
+                        let temp = Rating.split(" ");
+                        reviewJson.starRating = temp[1];
+
+                        var reviewObj = new ReviewInput();
+                        reviewObj.client = reviewJson.fullName;
+                        reviewObj.date = new Date();
+                        reviewObj.stars = parseInt(reviewJson.starRating);
+                        reviewObj.directoryId = 'Google';
+                        reviewObj.text = reviewJson.postReview;
+
+                        this.createReview(reviewObj);
+
+                    }
+
+                });
+
+                browser.close(); // Close the Browser...
+
+            } catch (err) {
+                console.log(err);
+            }
+
+
+        };
+
+        scrape();
+
+    }
 }
