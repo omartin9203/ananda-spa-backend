e119cfafee3d4028d77a1bcfaa5b112a2cbf9547
Roilan <armenterosroilan@gmail.com>
2020-06-11 15:46:27 -0400

    adding googleapis module

--- src/application/common/services/calendar/calendar.event.service.ts
+++ src/application/common/services/calendar/calendar.event.service.ts
@@ -3,6 +3,7 @@ import { ResourceService } from '../../../core/services/resource.service';
 import { CalendarEventDto } from '../../dtos/dtos/calendar/calendar.event.dto';
 import { CalendarEventRepository } from '../../../../infrastructure/common/repositories/calendar.event.repository';
 import { QueryBuilderService } from '../../../../infrastructure/core/services/query-builder.service';
+import {google} from 'googleapis';
 
 @Injectable()
 export class CalendarEventService extends ResourceService<CalendarEventDto> {
