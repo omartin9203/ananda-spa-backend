198825e5185e19ff3617c192f3d426557e1d4e95
Roilan <armenterosroilan@gmail.com>
2020-06-21 08:56:28 -0400

    apiKey token

--- src/application/common/controllers/review.controller.ts
+++ src/application/common/controllers/review.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Logger, Post, Req } from '@nestjs/common';
+import { Body, Controller, Headers, Logger, Post, Req } from '@nestjs/common';
 import { ReviewService } from '../services/review/review.service';
 import { ReviewSettingService } from '../services/settings/review-settings.service';
 import { ReviewInput } from '../dtos/inputs/review/review.input';
@@ -6,6 +6,7 @@ import { UserService } from '../services/user/user.service';
 import { AccreditedInputType } from '../dtos/inputs/review/accredited/accredited.input';
 import { ColorSettingService } from '../services/settings/color-setting.service';
 import { FeedbackInputType } from '../dtos/inputs/review/feedback/feedback.input';
+import { API_KEY } from '../../../constants';
 
 @Controller('review')
 export class ReviewController {
@@ -16,33 +17,37 @@ export class ReviewController {
   private readonly logger = new Logger(ReviewController.name);
   @Post('feedback')
   async saveFeedbackReview(
+    @Headers('token') apiKey,
     @Body() feedback: FeedbackInputType,
   ) {
     try {
-      const directoryId = (await this.reviewSettingService.getAll(0, 10))
-        .items.find(x => x.directoryName.toLocaleLowerCase().startsWith('ananda'))?.id;
-      if (directoryId) {
-        const colorId = (await this.colorSettingService.findOne({colorId: feedback.facialistId}))?.id;
-        const review = {
-          client: feedback.clientId,
-          date: new Date(),
-          text: feedback.extractions,
-          stars: feedback.rating,
-          directoryId,
-        } as ReviewInput;
-        if (colorId) {
-          const employee = (await this.userService.findOne({colorId}))?.id;
-          if (employee) {
-            review.accredited = {
-              accreditedDate: new Date(),
-              accreditedToEmployee: true,
-              employeeId: employee,
-            } as AccreditedInputType;
+      if (apiKey === API_KEY) {
+        const directoryId = (await this.reviewSettingService.getAll(0, 10))
+          .items.find(x => x.directoryName.toLocaleLowerCase().startsWith('ananda'))?.id;
+        if (directoryId) {
+          const colorId = (await this.colorSettingService.findOne({colorId: feedback.facialistId}))?.id;
+          const review = {
+            client: feedback.clientId,
+            date: new Date(),
+            text: feedback.extractions,
+            stars: feedback.rating,
+            directoryId,
+          } as ReviewInput;
+          if (colorId) {
+            const employee = (await this.userService.findOne({colorId}))?.id;
+            if (employee) {
+              review.accredited = {
+                accreditedDate: new Date(),
+                accreditedToEmployee: true,
+                employeeId: employee,
+              } as AccreditedInputType;
+            }
           }
+          await this.reviewService.createReview(review);
         }
-        await this.reviewService.createReview(review);
+      } else {
+        this.logger.debug('authorization filed');
       }
-
     } catch (e) {
       this.logger.debug(e);
     }
